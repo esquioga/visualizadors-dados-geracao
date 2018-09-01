a6908f7649273991711f43b7892ce473c1807a35
Joao Esquioga <joaoesquioga@gmail.com>
2018-09-01 17:52:17 -0300

    Dynamic collection update

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
-from data_access import getOutorgas
+from data_access import getOutorgas, upsertOutorgaInfo
+import datetime as dt
 
 app = Flask(__name__)
+upsertOutorgaInfo()
+lastUpdate = dt.datetime.now()
 
 @app.route("/")
 def index():
-	return render_template('index.html', infos=getOutorgas())
+	timeDelta = dt.datetime.now() - lastUpdate
+	outorgas = getOutorgas()
+	if (timeDelta.days > 29) or (not outorgas):
+		upsertOutorgaInfo()
+		outorgas = getOutorgas()	
+	return render_template('index.html', infos=outorgas)
 
 	
 	
