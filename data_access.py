75639d36d5e64b6c6f609bfd7abeb188fe35b89f
Joao Esquioga <joaoesquioga@gmail.com>
2018-09-01 17:14:33 -0300

    Added bootstrap

--- data_access.py
+++ data_access.py
@@ -9,10 +9,7 @@ collection = db.dados_outorga
 def getOutorgaJSON():
 	with urllib.request.urlopen("http://www.aneel.gov.br/dados/relatorios?p_p_id=dadosabertos_WAR_dadosabertosportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=gerarGeracaoDistribuidaJSON&p_p_cacheability=cacheLevelPage&p_p_col_id=column-2&p_p_col_count=1") as url:
 		data = json.loads(url.read().decode('latin-1'))
-		#for i in range(len(data)):
-		#	info = Info(data[i]['ideGeracaoDistribuida'], data[i]['nomGeracaoDistribuida'], str(data[i]['mesReferencia']) + '-' + str(data[i]['anoReferencia']), data[i]['mdaPotenciaInstaladakW'], data[i]['dthProcessamento'])
-		#	Infos.append(info)
-		return data#Infos
+		return data
 
 def upsertOutorgaInfo():
 	collection = db.dados_outorga
@@ -26,6 +23,7 @@ def getOutorgas():
 	infosOutorgas = []
 	for document in documents:
 		outorga = Info(document['ideGeracaoDistribuida'], document['nomGeracaoDistribuida'], str(document['mesReferencia']) + '-' + str(document['anoReferencia']), document['mdaPotenciaInstaladakW'], document['dthProcessamento'])
+		infosOutorgas.append(outorga)
 	return infosOutorgas	
 
 
